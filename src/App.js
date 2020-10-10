a0d400decc75766613fd7e6bd632926253abea45
Riku Tsuzuki <56225600+lune46@users.noreply.github.com>
2020-10-10 15:25:16 +0900

    routing した！

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+import Top from './screens/Top';
+import Quiz from './screens/Quiz';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      aaaaa
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Top} />
+          <Route path="/quiz" component={Quiz} />
+          <Route render={() => <h4>not found...</h4>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
